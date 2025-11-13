2564d50b81b400ca4247889f7d46135b5fa7bd86
hjh <ktorn21@naver.com>
2025-11-13 23:20:58 +0900

    초기 설정

---
+++ react-tsx/src/App.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import logo from "./logo.svg";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+    text-align: center;
+`;
+
+const Header = styled.div`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+`;
+
+function App() {
+    return (
+        //<div className="App">
+        <Container>
+            <Header>
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </Header>
+        </Container>
+        //</div>
+    );
+}
+
+export default App;

722ad0082d7f5eb359343a5acbdb3db34ca2953b
hjh <ktorn21@naver.com>
2025-11-14 22:57:04 +0900

    todo list UI 추가

--- react-tsx/src/App.tsx
+++ react-tsx/src/App.tsx
@@ -1,42 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
-import styled from "@emotion/styled";
-
-const Container = styled.div`
-    text-align: center;
-`;
-
-const Header = styled.div`
-    background-color: #282c34;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: white;
-`;
+import { Layout } from "./layouts/Layout";
+import { Example1 } from "./pages/Example1";
 
 function App() {
     return (
-        //<div className="App">
-        <Container>
-            <Header>
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </Header>
-        </Container>
-        //</div>
+        <Layout>
+            <Example1 />
+        </Layout>
     );
 }
 

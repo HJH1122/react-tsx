d5f451289436189ab65be1dae19a831170b1eeec
hjh <ktorn21@naver.com>
2025-11-15 17:58:37 +0900

    페이지 라우팅

--- react-tsx/src/App.tsx
+++ react-tsx/src/App.tsx
@@ -1,11 +1,36 @@
 import React from "react";
 import { Layout } from "./layouts/Layout";
 import { Example1 } from "./pages/Example1";
+import styled from "@emotion/styled";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Example2 } from "./pages/Example2";
+import { About } from "./pages/About";
+
+const NotFound = styled.div`
+    text-align: center;
+`;
 
 function App() {
     return (
         <Layout>
-            <Example1 />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/example1" element={<Example1 />} />
+                <Route path="/example2" element={<Example2 />} />
+
+                <Route
+                    path={"*"}
+                    element={
+                        <>
+                            <NotFound>
+                                404 <br /> NOT FOUND
+                            </NotFound>
+                        </>
+                    }
+                />
+            </Routes>
         </Layout>
     );
 }
---
+++ react-tsx/src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Our Website</h1>
+            <p>Home</p>
+        </div>
+    );
+};
